a33aa0fab6dc70d97638b92d530336b075eabc9e
Miaojun Zhang <junezhangmj@gmail.com>
2015-07-08 11:59:35 -0700

    first plot code

---
+++ plot1.R
@@ -0,0 +1,13 @@
+
+ setwd("~/Documents/Stat/Stat-R/exploratory-data-analysis" )
+ epc <- read.table ( file = "household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE,  na.strings = "?" )
+ epc.sub <- epc[ which (epc$Date == "2/1/2007" | epc$Date == "2/2/2007"), ] # or is | ????
+#
+#
+ par(mfrow = c(1, 1))
+ hist(epc.sub$Global_active_power, main = "Global Active Power", col = "red", xlim = c(0, 6),  ylim = c(0, 1200), xlab = "Global Active Power (kilowatts)")
+ 
+png(filename = "plot1.png",
+    width = 480, height = 480)
+ dev.off()
+#
